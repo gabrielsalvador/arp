4f0d1394e4453bcaf9c7b092368bada8f408f9ee
Gabriel Salvador <gabriel.carvalho.salvador@gmail.com>
2024-10-12 17:50:05 +0100

    added react-konva library

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import MainLayout from './layout/MainLayout'
+import { Stage, Layer, Rect, Circle } from 'react-konva'
+
+
 
 export const App = () => {
 
+    const [x, setX] = useState(0)
+    const [y, setY] = useState(0)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setX(x => x + 1)
+        }, 1000)
+        return () => clearInterval(interval)
+    })
+
+    const [width, setWidth] = useState(window.innerWidth)
+
     return <MainLayout topbar={undefined} leftSidebar={undefined} rightSidebar={undefined} leftFooter={undefined} rightFooter={undefined}>
-        <h1>Hello, World!</h1>
+        
+        <Stage width={window.innerWidth} height={window.innerHeight}>
+            <Layer>
+                <Circle x={x} y={200} stroke="red" fill="red" radius={5} draggable={true} onDragMove={(e) => {
+                    setX(e.target.x())
+                    setY(e.target.y())
+                }} />
+            </Layer>
+        </Stage>
+
     </MainLayout>
 }
