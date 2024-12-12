e79ce39ad11896dd5bc0e73b4e03439af34cfedf
Gabriel Salvador <gabriel.carvalho.salvador@gmail.com>
2024-12-12 22:10:46 +0000

    Implement StepSequencer and PlayStopButton components with logging functionality

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,51 @@
-import React, { useEffect, useState } from 'react'
-import MainLayout from './layout/MainLayout'
-import { Stage, Layer, Rect, Circle } from 'react-konva'
-import PianoRoll from './components/PianoRoll';
 import "../src/index.css"
+import React, { useState } from "react";
+import { StepSequencer } from "./components/StepSequencer"; 
 
+const N_STEPS = 16;
+const steps = Array.from({ length: N_STEPS }, (_, index) => ({
+    pitch: index % 12,
+    velocity: 100,
+    duration: 100,
+}));
 
 export const App = () => {
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [log, setLog] = React.useState<string[]>([]);
+
+
+    return  <div className="flex flex-col items-center">
+        <h1 className="text-center">Arp++</h1>
+        <span className="text-center">
+            <PlayStopButton isPlaying={isPlaying} setIsPlaying={setIsPlaying} />
+        </span>
+        <span className="text-center mt-3"></span>
+        <StepSequencer  setLog={setLog} />
+
+        <ul>
+            <h1>Logs</h1>
+            {log.map((message, index) => (
+                <li key={index}>{JSON.stringify(message)}</li>
+            ))}
+        </ul>
+    </div>
+    }
 
 
 
-    return <MainLayout topbar={undefined} leftSidebar={undefined} rightSidebar={undefined} leftFooter={<PianoRoll />} rightFooter={undefined}>
-
-
-        <Stage width={window.innerWidth} height={window.innerHeight}>
-            <Layer>
-                <Circle x={200} y={200} stroke="red" fill="red" radius={5} draggable={true} onDragMove={(e) => {}} />
-            </Layer>
-        </Stage>
-        
-
-            
-    </MainLayout>
+const PlayStopButton = ({ isPlaying,setIsPlaying }) => {
+    
+    const handlePlayStop = () => {
+        // @ts-ignore
+        __postNativeMessage__("toogleplay").then((response) => {
+            const parsed = JSON.parse(response);
+            setIsPlaying(parsed.isPlaying);
+        });
+    };
+    
+    return (
+        <button onClick={handlePlayStop} className="border-2 border-black w-8 h-8">
+            {isPlaying ? "Stop" : "Play"}
+        </button>
+    );
 }
